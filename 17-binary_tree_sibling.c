b6f71037bdd6ba98c6e5a1a8972efc72f35381c5
Robert Santana <123roabel@gmail.com>
2024-01-10 23:53:08 -0400

    fix function 16-18

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,19 +6,17 @@
  *
  * Return: Pointer to the sibling node, or NULL if no sibling
  */
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return (NUL)L;
+		return (NULL);
 
-	if (node->parent->left != NULL && node->parent->right != NULL)
-	{
-		if (node->parent->left == node)
-			return (node->parent->right);
-		else
-			return (node->parent->left);
-	}
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
 
-	return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
+
